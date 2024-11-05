61ebeb496518c5476e31bec58e3349ff1f29c1ba
stiliksew <132008646+stiliksew@users.noreply.github.com>
2024-11-05 08:13:26 -0500

    Update shifter.py

--- shifter.py
+++ shifter.py
@@ -22,8 +22,8 @@ class Shifter:
         time.sleep(0)
         GPIO.output(p,0)
         
-#shifter_obj=Shifter(23,25,24)
-#shifter_obj.shiftByte(7)
+shifter_obj=Shifter(23,25,24)
+shifter_obj.shiftByte(7)
         
     #GPIO.output(latchPin, 1) # ping the latch pin to send register to output
     #time.sleep(0)
